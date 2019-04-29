3387b0debb4edbd4b3759671a572638b075cfb35
Diego Alejandro <diego.fc.1@hotmail.com>
2019-04-29 11:55:09 -0500

    definiendo la estructura principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PacienteComponent } from './pages/paciente/paciente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'paciente', component: PacienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from '../../.history/src/app/material/material.module_20190429114904';
+import { PacienteComponent } from './pages/paciente/paciente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PacienteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
